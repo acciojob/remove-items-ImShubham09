32068cd25a0595cc8adc91b42b9196072f685653
ImShubham09 <singhashubham612@gmail.com>
2024-12-27 22:05:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",function(){
+   let selectcolour = document.getElementById("colorSelect");
+   let removeBtn= document.querySelector("input[type='button']");
+
+   removeBtn.addEventListener("click",function(){
+    let selectedindex = selectcolour.selectedIndex;
+
+    if(selectedindex !== -1){
+        selectcolour.options[selectedindex].remove();
+    }
+   });
+
+});
